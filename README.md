6def6e59f7ea445592a588d8fda689d30ee07196
tooka6718 <34338648+tooka6718@users.noreply.github.com>
2022-12-13 10:48:56 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-vjwlob
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-vjwlob)
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="">
+    <input type="text" :value="name" @input="name = $event.target.value" />
+    <br />
+    <p>{{ name }}</p>
+    <div>{{ msg }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      name: 'test',
+    };
+  },
+
+  props: {
+    msg: String,
+  },
+};
+</script>
