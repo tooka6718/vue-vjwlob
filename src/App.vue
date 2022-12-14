4dde5f782f484362feaba51498ef0383fa298f89
tooka6718 <34338648+tooka6718@users.noreply.github.com>
2022-12-14 09:54:14 +0100

    3 files modified, 1 added

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,17 @@
 <template>
-  
-  <div class="flex min-h-screen w-screen flex-col justify-center items-center">
-  
-    <h2 class="font-bold mb-2 border p-4">Assignments</h2>
+  <div class="flex min-h-screen w-screen flex-col items-center">
+    <h2 class="font-bold mb-2 border p-4 my-4">Assignments</h2>
     <ul class="border p-4">
-      <li v-for="assignment in assignments" >
-
+      <li v-for="assignment in assignments">
         <label class="flex justify-between">
-
-          {{ assignment.name}}
-          <input type="checkbox" v-model="assignment.complete" class="ml-5">
-
+          {{ assignment.name }}
+          <input type="checkbox" v-model="assignment.complete" class="ml-5" />
         </label>
-
       </li>
     </ul>
-    <pre>
-      {{assignments}}
-    </pre>
-  </div>
 
+    <app-button></app-button>
+  </div>
 </template>
 
 <script>
@@ -27,21 +19,23 @@
 export default {
   name: 'App',
 
-  data(){
+  data() {
     return {
       assignments: [
-        {name: 'Finsh project', complete: false, id:1},
-        {name: 'Read chapter 4', complete: false, id:2},
-        {name: 'Turn in Homework', complete: false, id:3}
-      ]
-    }
+        { name: 'Finsh project', complete: false, id: 1 },
+        { name: 'Read chapter 4', complete: false, id: 2 },
+        { name: 'Turn in Homework', complete: false, id: 3 },
+      ],
+    };
   },
 
   components: {
     // HelloWorld,
+    'app-button': {
+      template: `
+        <button class="bg-gray-400 rounded px-5 py-2 mt-5">Siema</button>
+      `,
+    },
   },
 };
 </script>
-
-<style>
-
---
+++ src/App2.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="flex min-h-screen w-screen flex-col justify-center items-center">
+    <h2 class="font-bold mb-2 border p-4">Assignments</h2>
+    <ul class="border p-4">
+      <li v-for="assignment in assignments">
+        <label class="flex justify-between">
+          {{ assignment.name }}
+          <input type="checkbox" v-model="assignment.complete" class="ml-5" />
+        </label>
+      </li>
+    </ul>
+    <pre>
+      {{ assignments }}
+    </pre>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue';
+export default {
+  name: 'App',
+
+  data() {
+    return {
+      assignments: [
+        { name: 'Finsh project', complete: false, id: 1 },
+        { name: 'Read chapter 4', complete: false, id: 2 },
+      ],
+    };
+  },
+
+  components: {
+    // HelloWorld,
+  },
+};
+</script>
