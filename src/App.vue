d2e7ff77afad1c8a387f19439b8a28457f831f37
tooka6718 <34338648+tooka6718@users.noreply.github.com>
2022-12-14 10:21:44 +0100

    1 file modified, 1 added

--- src/App.vue
+++ src/App.vue
@@ -33,9 +33,7 @@ export default {
     // HelloWorld,
     'app-button': {
       template: `
-        <button class="bg-gray-400 rounded px-5 py-2 mt-5">
-          <slot />
-        </button>
+        
       `,
     },
   },
---
+++ src/components/AppButton.vue
@@ -0,0 +1,5 @@
+<template>
+  <button class="bg-gray-400 rounded px-5 py-2 mt-5">
+    <slot />
+  </button>
+</template>
