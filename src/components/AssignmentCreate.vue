fc406349e5c8ad3058025ec4d11f8225f98ee09f
tooka6718 <34338648+tooka6718@users.noreply.github.com>
2022-12-15 11:55:35 +0100

    1 file modified

--- src/components/AssignmentCreate.vue
+++ src/components/AssignmentCreate.vue
@@ -1,41 +1,31 @@
 <template>
-  
-    <form class="" @submit.prevent="add">
-      <div class="border border-gray-400 text-black">
-        <input
-          placeholder="New Assignment."
-          class="p-2"
-          v-model="newAssignment"
-        />
-        <button type="submit" class="p-2 border-l">Add</button>
-    
-    </form>
-  </divide>
+  <form class="" @submit.prevent="add">
+    <div class="border border-gray-400 text-black flex justify-between">
+      <input
+        placeholder="New Assignment."
+        class="p-2"
+        v-model="newAssignment"
+      />
+      <button type="submit" class="border-l p-2">Add</button>
+    </div>
+  </form>
 </template>
-
 
 <script>
 export default {
   data() {
     return {
-      
       newAssignment: '',
-
     };
   },
 
   props: {},
 
   methods: {
-    
-    add(){
-
+    add() {
       this.$emit('add', this.newAssignment);
-      this.newAssignment= '';
-
+      this.newAssignment = '';
     },
-
-
   },
 };
 </script>
