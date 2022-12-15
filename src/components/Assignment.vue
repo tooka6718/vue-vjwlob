a784d2bd360f1d0c33d91798d75680e74b5ffde8
tooka6718 <34338648+tooka6718@users.noreply.github.com>
2022-12-15 10:06:31 +0100

    2 files modified

--- src/components/Assignment.vue
+++ src/components/Assignment.vue
@@ -1,8 +1,8 @@
 <template>
-  <li>
-    <label>
+  <li class="p-3">
+    <label class="flex justify-between items-center">
       {{ assignment.name }}
-      <input type="checkbox" v-model="assignment.complete" />
+      <input type="checkbox" v-model="assignment.complete" class="ml-3" />
     </label>
   </li>
 </template>
--- src/components/Assignments.vue
+++ src/components/Assignments.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="flex w-screen flex-col items-center mb-5">
+  <div class="flex w-screen flex-col items-center mb-5" >
     <h2 class="font-bold mb-2 border p-4 my-4" v-show="assignments.length">
       {{ title }}
     </h2>
-    <ul class="border p-4">
+    <ul class="border divide-y divide-gray-400">
       <Assignment 
         v-for="assignment in assignments"
         :key="assignment.id" 
