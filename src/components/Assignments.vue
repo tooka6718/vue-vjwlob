0bb75a5e69ed8e5018b8e93026e94c5a273b036f
tooka6718 <34338648+tooka6718@users.noreply.github.com>
2022-12-15 15:31:05 +0100

    2 files modified

--- src/components/Assignments.vue
+++ src/components/Assignments.vue
@@ -1,6 +1,7 @@
 <template>
   <h2 class="font-bold mb-2 border p-3 my-4" v-show="assignments.length">
     {{ title }}
+    <span class="text-red-500">({{assignments.length}})</span>
   </h2>
   <ul class="border divide-y divide-gray-400 mb-2">
     <Assignment
