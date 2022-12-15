896e002c1144f017627891819b618f0dbe0d2c7f
tooka6718 <34338648+tooka6718@users.noreply.github.com>
2022-12-15 06:36:02 +0100

    3 files modified

--- src/components/AppButton.vue
+++ src/components/AppButton.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <button class="bg-gray-400 hover:bg-gray-200 rounded px-5 py-2 mt-5">
+    <button
+      :class="{
+        'rounded px-5 py-2 mt-5': true,
+        'bg-blue-200 hover:bg-blue-400': type === 'primary',
+        'bg-purple-200 hover:bg-purple-400': type === 'secondary',
+        'bg-gray-200 hover:bg-gray-400': type === 'muted',
+        'is-loading': processing,
+      }"
+      :disabled="processing"
+    >
       <slot />
     </button>
   </div>
@@ -9,11 +18,18 @@
 <script>
 export default {
   data() {
-    return {
-      processing: true,
-    };
+    return {};
   },
 
-  props: {},
+  props: {
+    type: {
+      type: String,
+      default: 'primary',
+    },
+    processing: {
+      type: Boolean,
+      default: false,
+    },
+  },
 };
 </script>
